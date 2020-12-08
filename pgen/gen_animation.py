45e1c121f273d13de8fb57d9d94877f5accebd3c
RiveSunder <RiveSunder@protonmail.com>
2020-12-08 11:49:32 -0700

    add render option to gen_animation

--- pgen/gen_animation.py
+++ pgen/gen_animation.py
@@ -11,7 +11,8 @@ import skimage
 import skimage.io
 import matplotlib.pyplot as plt
 
-def gen_animation(env_name, generator_file, latent_steps=10, save_figs=False):
+def gen_animation(env_name, generator_file, latent_steps=10, save_figs=False,\
+        render=False):
 
     env = gym.make(env_name)
 
@@ -28,10 +29,14 @@ def gen_animation(env_name, generator_file, latent_steps=10, save_figs=False):
     
     # walk the latent space, dimension 0
     latent_range = 1.
-    latent_walk = np.arange(-latent_range/2, latent_range/2, latent_range / latent_steps)[:,np.newaxis]
+    latent_walk = np.arange(-latent_range/2, latent_range/2, \
+            latent_range / latent_steps)[:,np.newaxis]
     
     fitness_landscape = []
     epds = 8
+
+    if render and "BulletEnv" in env_name:
+        env.render()
 
     for latent_walk0 in latent_walk:
         for latent_walk1 in latent_walk:
@@ -57,14 +62,18 @@ def gen_animation(env_name, generator_file, latent_steps=10, save_figs=False):
                     obs, reward, done, info = env.step(act)
                     epd_reward += reward
                     
+                    if render and "BulletEnv" not in env_name:
+                        env.render()
+
                     if epd == 0 and save_figs:
                         env.unwrapped._render_width = 640
                         env.unwrapped._render_height = 480
                         img = env.render(mode="rgb_array")
-                        skimage.io.imsave("./results/figs/{}_step{}_latent{}.png".format(\
-                                        env_name, str(step).zfill(3), \
-                                        str(int(10*latent_walk0)) + str(int(latent_walk1))),\
-                                        np.uint8(img))
+                        skimage.io.imsave("./results/figs/{}_step{}_l{}.png"\
+                                .format(env_name, str(step).zfill(3), \
+                                str(int(10*latent_walk0)) \
+                                + str(int(10*latent_walk1))),\
+                                np.uint8(img))
                     step += 1
             fitness_landscape.append(epd_reward/epds)
             print("reward : {}".format(epd_reward), " latent space ", latent_space)
@@ -82,6 +91,7 @@ if __name__ == "__main__":
     parser.add_argument("-g", "--generator_file", type=str, default="results/InvPend_generators.pickle")
     parser.add_argument("-l", "--latent_steps", type=int, default=3)
     parser.add_argument("-s", "--save_figs", type=bool, default=False)
+    parser.add_argument("-r", "--render", type=bool, default=False)
 
     args = parser.parse_args()
 
@@ -89,4 +99,4 @@ if __name__ == "__main__":
 
     generator_file = args.generator_file
 
-    gen_animation(env_name, generator_file, latent_steps=args.latent_steps, save_figs=args.save_figs)
+    gen_animation(env_name, generator_file, latent_steps=args.latent_steps, save_figs=args.save_figs, render=args.render)
